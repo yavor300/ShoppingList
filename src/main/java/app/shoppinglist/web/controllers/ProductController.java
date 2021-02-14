a0bedcd4cae66fd8a48ca2f0b3a44aa679db974d
Yavor <yavor300@gmail.com>
2021-02-14 11:38:25 +0200

    product add form with validations

--- src/main/java/app/shoppinglist/web/controllers/ProductController.java
+++ src/main/java/app/shoppinglist/web/controllers/ProductController.java
@@ -1,14 +1,40 @@
 package app.shoppinglist.web.controllers;
 
+import app.shoppinglist.domain.models.binding.ProductAddBindingModel;
+import app.shoppinglist.domain.models.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+
     @GetMapping("/add")
-    public String add() {
+    public String add(Model model) {
+        if (!model.containsAttribute("productAddBindingModel")) {
+            model.addAttribute("productAddBindingModel", new ProductAddBindingModel());
+        }
         return "product-add";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid @ModelAttribute ProductAddBindingModel productAddBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("productAddBindingModel", productAddBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.productAddBindingModel", bindingResult);
+            return "redirect:add";
+        }
+
+        return "redirect:/";
     }
 }
